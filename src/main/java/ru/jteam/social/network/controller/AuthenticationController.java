e7789258a24db4f32a1cdb76fe1dea67c0a0550c
Shagbark <protsko@yamoney.ru>
2018-04-06 17:20:52 +0300

    Added login and registration pages.

--- src/main/java/ru/jteam/social/network/controller/AuthenticationController.java
+++ src/main/java/ru/jteam/social/network/controller/AuthenticationController.java
@@ -7,11 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @author protsko on 22.03.18
  */
 @Controller
-public class LoginController {
+public class AuthenticationController {
 
     @GetMapping(value = "/login")
     public String loginPage() {
         return "login";
     }
+
+    @GetMapping(value = "/registration")
+    public String signUpPage() {
+        return "registration";
+    }
+
 
 }
