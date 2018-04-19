2668271d9830df194ab73dfbcfbe6fb385184635
protsko <protsko@yamoney.ru>
2018-04-19 11:25:34 +0300

    Added unique contraint validator test.

--- src/test/groovy/ru/jteam/social/network/validation/contraint/validator/UniqueConstraintValidatorTest.groovy
+++ src/test/groovy/ru/jteam/social/network/validation/contraint/validator/UniqueConstraintValidatorTest.groovy
@@ -4,13 +4,13 @@ import org.springframework.context.ApplicationContext
 import ru.jteam.social.network.validation.UniqueValidator
 import ru.jteam.social.network.validation.annotation.Unique
 import spock.lang.Specification
+
 /**
  * @author protsko on 19.04.18
  */
 class UniqueConstraintValidatorTest extends Specification {
 
     private final ApplicationContext context = Mock(ApplicationContext.class)
-    private final Data object = new Data()
 
     private UniqueConstraintValidator validator
 
@@ -21,24 +21,34 @@ class UniqueConstraintValidatorTest extends Specification {
 
     def "initialize() - throw exception, because validator is interface"() {
         given:
-            Unique unique = getAnnotation(true)
-//            println unique.uniqueValidator()
-        expect:
-            validator.initialize(null)
+            Unique unique = getInvalidAnnotation()
+        when:
+            validator.initialize(unique)
+        then:
+            thrown IllegalArgumentException.class
     }
 
-    def getAnnotation(boolean validAnnotation) {
-        object.class.fields.each {
-            println it.type
-            if (it.getAnnotation(Unique.class) == null) {
-                println it
-            }
-        }
-        return null
-//        data.class.fields.find({ field ->
-//            println field.getName()
-////            field.getAnnotation(Unique.class).uniqueValidator() == UniqueValidator.class
-//        })
+    def "initialize() - initialize validator"() {
+        given:
+            Unique unique = getValidAnnotation()
+        when:
+            validator.initialize(unique)
+        then:
+            1 * context.getBean(unique.uniqueValidator())
+    }
+
+    def getInvalidAnnotation() {
+        Data.class.declaredFields.find {
+            Unique annotation = it.getAnnotation(Unique.class)
+            annotation != null && annotation.uniqueValidator().isInterface()
+        }.getAnnotation(Unique.class)
+    }
+
+    def getValidAnnotation() {
+        Data.class.declaredFields.find {
+            Unique annotation = it.getAnnotation(Unique.class)
+            annotation != null && !annotation.uniqueValidator().isInterface()
+        }.getAnnotation(Unique.class)
     }
 
     class Data {
@@ -50,7 +60,7 @@ class UniqueConstraintValidatorTest extends Specification {
 
     class UniqueTestValidator implements UniqueValidator {
         @Override
-        public boolean validate(Object value) {
+        boolean validate(Object value) {
             return false
         }
     }
